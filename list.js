46aaab1abd7aa536ab3443809afdc7b3ad741280
Lior Shtivelman <lior.shtivelman@gmail.com>
2019-06-30 12:40:51 +0300

    added window managment

--- list.js
+++ list.js
@@ -1,54 +1,66 @@
-document.addEventListener('DOMContentLoaded', function() {
-	cAllTabs.init();
-	cAllTabs.displayList();
-})
+(function main() {
+	let currentWindowId = null;
+	let windowCounter = 0;
 
-document.addEventListener('unload', function() {
-	cAllTabs.unregisterAndCleanUp();
- })
+	const init = function() {
+		registerToClickEvent();
+	};
 
-const cAllTabs = {
-	init : function() {
-		this.registerToClickEvent();
-	},
-
-	displayList : function () {
+	const displayList = function() {
 		const tabList = document.querySelector('.tab-list');
-		const rowFragment = document.createDocumentFragment();
 
-		chrome.tabs.query({}, tabs => {
-			tabs.forEach(tab => {
-				let active = tab.active ? 'active' : '';
-				const speakerVisible = !tab.audible ? 'hidden' : '' ;
+		chrome.windows.getAll({ populate: true }, function(windows) {
+			windows.forEach(chromeWindow => {
+				const tabRowFragment = document.createDocumentFragment();
+				chromeWindow.tabs.forEach(tab => {
+					tabRowFragment.appendChild(buildTabRow({ tab }));
+				});
 
-				let speakerImg = 'display:none';
-				let mutedSpeakerImg = 'display:none';
-
-				if (tab.audible) {
-					if (tab.mutedInfo.muted) {
-						speakerImg = 'display:none';
-						mutedSpeakerImg = 'display:block';
-					}	
-					else {
-						speakerImg = 'display:block';
-						mutedSpeakerImg = 'display:none';
-					}
+				if (windows.length > 1) {
+					const group = buildGroup({ chromeWindow, tabRowFragment });
+					tabList.appendChild(group);
+				} else {
+					tabList.appendChild(tabRowFragment);
 				}
-
-				rowFragment.appendChild(this.buildTabRow({tab, active, speakerVisible, speakerImg, mutedSpeakerImg}));
 			});
-
-			tabList.appendChild(rowFragment);
 		});
-	},
+	};
 
-	buildTabRow: function({tab, active, speakerImg, mutedSpeakerImg, speakerVisible}) {
+	const buildGroup = function({ chromeWindow, tabRowFragment }) {
+		const group = document.createElement('div');
+		group.className = 'group';
+
+		const groupTitle = document.createElement('div');
+		groupTitle.className = 'group-title';
+		currentWindowId = chromeWindow.tabs[0].windowId;
+		windowCounter++;
+		groupTitle.innerText = `window ${windowCounter}`;
+		group.className = 'group';
+		group.dataset.groupId = currentWindowId;
+		group.appendChild(groupTitle);
+		group.appendChild(tabRowFragment);
+		return group;
+	};
+
+	const buildTabRow = function({ tab }) {
+		const active = tab.active ? 'active' : '';
+		const speakerVisible = !tab.audible ? 'hidden' : '';
+
+		let speakerImg = 'display: none';
+		let mutedSpeakerImg = 'display:none';
+
+		if (tab.audible) {
+			speakerImg = tab.mutedInfo.muted ? 'display:none' : 'display:block';
+			mutedSpeakerImg = tab.mutedInfo.muted ? 'display:block' : 'display:none';
+		}
+
 		const tabRow = document.createElement('div');
 		tabRow.className = `tab-row ${active}`;
 		tabRow.dataset.tabId = tab.id;
+		tabRow.dataset.windowId = tab.windowId;
 
 		const favIcon = document.createElement('div');
-		favIcon.className = 'favicon';	
+		favIcon.className = 'favicon';
 
 		const favIconImage = document.createElement('img');
 		favIconImage.src = tab.favIconUrl;
@@ -69,17 +81,17 @@ const cAllTabs = {
 		speakerImage.style = speakerImg;
 
 		speakerSpan.append(speakerImage);
-		
+
 		const mutedSpeakerImgElement = document.createElement('img');
 		mutedSpeakerImgElement.src = '../images/icons8-No Audio-16.png';
 		mutedSpeakerImgElement.alt = 'tab is muted';
 		mutedSpeakerImgElement.dataset.type = 'speaker';
 		mutedSpeakerImgElement.style = mutedSpeakerImg;
-		
+
 		speakerSpan.append(speakerImage);
 		speakerSpan.append(mutedSpeakerImgElement);
 		tabTitle.appendChild(speakerSpan);
-		
+
 		const tabDesc = document.createElement('span');
 		tabDesc.className = 'tab-desc';
 		tabDesc.innerText = tab.title;
@@ -100,97 +112,121 @@ const cAllTabs = {
 		tabRow.appendChild(closeButtonDiv);
 
 		return tabRow;
-	},
+	};
 
-	registerToClickEvent : function(){
+	const registerToClickEvent = function() {
 		const tabList = document.querySelector('.tab-list');
-		tabList.addEventListener('click', cAllTabs.handleClickEvent.bind(this));
-	},
+		tabList.addEventListener('click', handleClickEvent);
+	};
 
-	handleClickEvent: function(event){
-		const tabId = this.getTabId(event);
+	const handleClickEvent = function(event) {
+		const { tabId, windowId } = getTabData(event);
 		const tagName = event.target.tagName.toLowerCase();
 		const type = event.target.dataset.type;
 
 		if (tagName === 'img' && type === 'speaker') {
-			this.toggleMute.bind(this, tabId)();
+			toggleMute(tabId);
 			return;
 		}
 
-		if ((tagName === 'img' || tagName === 'div') &&  type === 'closeButton') {
-			this.removeTabFromList(tabId);
-			this.closeTab(tabId);
+		if ((tagName === 'img' || tagName === 'div') && type === 'closeButton') {
+			removeTabFromList(tabId);
+			closeTab(tabId);
+		} else {
+			setActiveTab({ tabId, windowId });
 		}
-		else {
-			this.setActiveTab(tabId);
-		}
-	},
+	};
 
-	toggleMute : function(tabId) {
+	const toggleMute = function(tabId) {
 		chrome.tabs.get(tabId, function(tabData) {
 			const muted = !tabData.mutedInfo.muted;
-			chrome.tabs.update(tabId, {muted:muted});
-			this.toggleMuteIcon(tabId, muted);
-		}.bind(this));
-	},
+			chrome.tabs.update(tabId, { muted: muted });
+			toggleMuteIcon(tabId, muted);
+		});
+	};
 
-	setActiveTab : function(tabId){
-		chrome.tabs.update(tabId, {active:true});
-	},
+	const setActiveTab = function({ tabId, windowId }) {
+		// if (windowId && windowId !== selectedWindowId) {
+		chrome.windows.update(windowId, { focused: true }, function() {
+			selectedWindowId = windowId;
+		});
 
-	unregisterAndCleanUp : function(){
+		chrome.tabs.update(tabId, { active: true });
+		// } else {
+		// 	chrome.tabs.update(tabId, { active: true });
+		// }
+	};
+
+	const unregisterAndCleanUp = function() {
 		document.querySelector('.tab-list').removeEventListener('click', cAllTabs.handleClickEvent);
+	};
 
-	},
-
-	getTabId : function(event) {
+	const getTabData = function(event) {
 		let currentElement = event.target;
 		let elementType = currentElement.tagName.toLowerCase();
 
-		while(elementType !== 'div' || (elementType === 'div' && !currentElement.classList.contains('tab-row'))) {
+		while (elementType !== 'div' || (elementType === 'div' && !currentElement.classList.contains('tab-row'))) {
 			currentElement = currentElement.parentNode;
 			elementType = currentElement.tagName.toLowerCase();
 		}
 
-		return parseInt(currentElement.dataset.tabId);
-	},
+		return {
+			tabId: parseInt(currentElement.dataset.tabId),
+			windowId: parseInt(currentElement.dataset.windowId),
+		};
+	};
 
-	closeTab: function(tabId) {
+	const closeTab = function(tabId) {
 		chrome.tabs.remove(tabId);
-	},
+	};
 
-	removeTabFromList : function(tabId) {
+	const removeTabFromList = function(tabId) {
 		const list = [...document.querySelectorAll('.tab-row')];
 		const tab = list.find(tab => parseInt(tab.dataset.tabId) === parseInt(tabId));
-		document.querySelector('.tab-list').removeChild(tab);
-	},
+		
+		const group = tab.parentElement;
 
-	toggleMuteIcon : function(tabId, muted) {
+		if (group && group.children.length > 1) {
+			group.removeChild(tab);
+		} else {
+			document.querySelector('.tab-list').removeChild(tab);
+		}
+	};
+
+	const toggleMuteIcon = function(tabId, muted) {
 		const list = [...document.querySelectorAll('.tab-row')];
 		const tab = list.find(tab => parseInt(tab.dataset.tabId) === parseInt(tabId));
 
 		const tabTitle = tab.children[1];
 		if (muted) {
-			tabTitle.children[0].children[0].style.display = "none";
-			tabTitle.children[0].children[1].style.display = "block";
+			tabTitle.children[0].children[0].style.display = 'none';
+			tabTitle.children[0].children[1].style.display = 'block';
+		} else {
+			tabTitle.children[0].children[0].style.display = 'block';
+			tabTitle.children[0].children[1].style.display = 'none';
 		}
-		else {
-			tabTitle.children[0].children[0].style.display = "block";
-			tabTitle.children[0].children[1].style.display = "none";
-		}
-	
-		//document.querySelector('.tab-list').removeChild(tab);
-	},
+	};
 
-	handleSettingsClick : function() {
+	const handleSettingsClick = function() {
 		const display = document.querySelector('.settings-content').style.display;
 		if (display === 'none' || display === '') {
 			document.querySelector('.settings-content').style.display = 'block';
 			document.querySelector('.tab-list').style.display = 'none';
-		}
-		else {
+		} else {
 			document.querySelector('.settings-content').style.display = 'none';
 			document.querySelector('.tab-list').style.display = 'block';
 		}
-	}
-}
+	};
+
+	// register to dom events
+	// ---------------------------------------------------------
+	document.addEventListener('DOMContentLoaded', function() {
+		init();
+		displayList();
+	});
+
+	document.addEventListener('unload', function() {
+		unregisterAndCleanUp();
+	});
+	// ---------------------------------------------------------
+})();
