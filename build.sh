ee1fd5a0fe5d47ea5a61076c93f9c9b66c602b71
Lior <lior.shtivelman@gmail.com>
2019-09-20 16:16:34 +0300

    create a zip version using script

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf version.zip
+
+zip version.zip -r ./css/ ./images/ 440x280.png 440x280_2.png 440x280_3.png icon_128.png if__Select_Webpage_1153753.png if_Tabs_342927.png index.html main.min.js manifest.json README.md screenshot1.JPG
