2da4f7ea8d04a6846d4acd3caca69910dc272630
Lior <lior.shtivelman@gmail.com>
2019-09-28 19:00:11 +0300

    minor build changes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
+rm -rf ./version/XXX.zip
 
-rm -rf version.zip
+mkdir version
 
-zip version.zip -r ./css/ ./images/ 440x280.png 440x280_2.png 440x280_3.png icon_128.png if__Select_Webpage_1153753.png if_Tabs_342927.png index.html main.min.js manifest.json README.md screenshot1.JPG
+zip ./version/XXX.zip -r App/App.min.js App/consts.min.js css  440x280.png 440x280_2.png 440x280_3.png icon_128.png if__Select_Webpage_1153753.png if_Tabs_342927.png index.html main.min.js manifest.json README.md screenshot1.JPG
