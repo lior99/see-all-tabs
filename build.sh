6c2e38a833c6bb23211ef3f8e5041d0bd3a248d4
Lior Shtivelman <lior.shtivelman@gmail.com>
2020-07-11 17:32:05 +0300

    updated build.sh to create versions for store

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ rm -rf ./version
 
 mkdir version
 
-./build.sh
+zip version/version.zip css/*.* images/*.* 440x280.png 440x280_2.png 440x280_3.png icon_128.png if__Select_Webpage_1153753.png if_Tabs_342927.png index.html main.js manifest.json README.md screenshot1.JPG
